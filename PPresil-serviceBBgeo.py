2929ee24ce33f0f66492fbc034ef3f9893dc1532
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-08-02 11:13:28 +0200

    script definitivo

--- PPresil-serviceBBgeo.py
+++ PPresil-serviceBBgeo.py
@@ -43,10 +43,11 @@ MemFilePath="/Tmp/MSaviFeat"
 path="Dati_SatPeneda"
 file="MSAVI_AOI_MASKED_STACK_NEW"
 west,south,east,north =read_csv("BBfile.txt", index_col=None, header=None,delim_whitespace=True).values.flatten().tolist()
+
 if not(south==west==north==east==0):
     ds = gdal.Open(path+"/"+file+".envi")
-    ds = gdal.Translate(path+"BB_"+file, ds, projWin = [south,west,north,east])
-    ds = None
+    sd = gdal.Translate(path+"/"+"BB_"+file+".envi", path+"/"+file+".envi", projWin=[west, north, east,south],format="ENVI")
+    #ds = None
     file="BB_"+file
 file=path+"/"+file
 #Output path
